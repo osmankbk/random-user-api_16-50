6cc8ec1dda451a35c12d759f8fc30acc311b0091
osmankbk <32382267+osmankbk@users.noreply.github.com>
2020-11-09 20:31:54 -0500

    random user fetch warm up project

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const personPic = document.querySelector('#photo');
+const personFirstName = document.querySelector('#first');
+const personLastName = document.querySelector('#last');
+const personLocation = document.querySelector('#street');
+const personPhone = document.querySelector('#phone');
+const personEmail = document.querySelector('#email');
+const button = document.querySelector('button');
+
+//Data fetching function.
+async function getPerson(url) {
+   try{
+    const request = await fetch(url);
+    const response = await request.json()
+    return response.results[0];
+} catch(error) {
+    error.status
+    }
+}
+console.log(getPerson(`https://randomuser.me/api/`))
+
+
+//Passing fetch data into DOM function.
+const fillInfo = (data) => {
+    const {
+        phone,
+        email,
+} = data;
+
+    personPic.src = data.picture.large;
+    personFirstName.textContent = data.name.first;
+    personLastName.textContent = data.name.last;
+    personLocation.textContent = data.location.street.name
+    personPhone.textContent = phone;
+    personEmail.textContent = email;
+}
+
+//Show data click event.
+button.addEventListener('click', () => {
+    getPerson(`https://randomuser.me/api/`).then(fillInfo);
+})
